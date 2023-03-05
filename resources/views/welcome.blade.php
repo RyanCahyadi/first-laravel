21a086179f85f980bec1c9120e53910290268985
RyanCahyadi <ryancahyadi11@gmail.com>
2023-03-05 09:16:55 +0700

    First

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Homepage</h1>
+        <a href="{{ route('about') }}">About</a>
+        <a href="{{ route('contact') }}">Contact</a>
+        <a href="{{ route('product') }}">Product</a>
+
+    </div>
+</body>
+
+</html>
